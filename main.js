729d533cc4192edbaf9042824d879ed227fecc31
kitschkai <kaileah@t-online.de>
2026-09-18 14:21:20 +0200

    Fix pirates section link formatting
    Removed an extra link entry from the pirates section.

--- main.js
+++ main.js
@@ -175,7 +175,7 @@ function showProjectPreview(key) {
       title: 'INTIMACY PIRATES',
       year: '2024',
       desc: 'Lecture Performance on a raft, starting on land with a short poetic introduction and activation of the audience. The text tells of anxieties and fear, anger, love and friendship and the capicity of letting go to meet anew. The work was created for the JETZT! Festival of Kulturregion Stuttgart, in cooperation with Kulturkabinett Stuttgart and performed three times at different stages of the raft’s travel. Reiterated in the course of the interdisciplinary Project "flood fictions / futurs immergés" by Libertalie Kollektiv, in Hamburg 2026. Duration 1-1,5 h.',
-      link: 'https://www.kontextwochenzeitung.de/schaubuehne/704/piratinnen-der-intimitaet-9750.html', 'https://libertalia-kollektiv.eu/futurs-immerges-flood-fictions-kick-off/',
+      link: 'https://www.kontextwochenzeitung.de/schaubuehne/704/piratinnen-der-intimitaet-9750.html',
       images: [
         { src: 'images/pirates_6.jpg', meta: 'Dokumentation, Hamburg 2026 (c) Libertalia Kollektiv' },
         { src: 'images/pirates_5.jpg', meta: 'Dokumentation, Hamburg 2026 (c) Libertalia Kollektiv' },
